89a0af35c111f5cf84a2b821de25dd7177fdffe4
ArnavSaikia <arnavsaikia817@gmail.com>
2026-03-30 09:58:00 +0530

    added to the userSchema and added pre-save middleware for auth

--- backend/models/userModel.js
+++ backend/models/userModel.js
@@ -1,4 +1,5 @@
 const mongoose = require("mongoose");
+const bcrypt = require('bcryptjs');
 
 const userSchema = new mongoose.Schema({
     username: {
@@ -8,12 +9,32 @@ const userSchema = new mongoose.Schema({
     },
     password: {
         type: String,
-        required: true
+        required: true,
+        select: false
     },
     steamId: {
         type: String,
         default: null
-    }
+    },
+    steamVerified: {
+        type: Boolean,
+        default: false,
+    },
 }, { timestamps: true });
 
-module.exports = mongoose.model("User", userSchema);
+userSchema.pre("save", async function (next) {
+    if(!this.isModified('password')) return next();
+
+    const salt = await bcrypt.genSalt(10);
+    this.password = await bcrypt.hash(this.password , salt);
+    next();
+});
+
+userSchema.methods.matchPassword = async function (enteredPassword) {
+    if (!this.password) return false;
+    return await bcrypt.compare(enteredPassword, this.password);
+};
+
+const User = mongoose.model("User", userSchema);
+
+module.exports = User;
