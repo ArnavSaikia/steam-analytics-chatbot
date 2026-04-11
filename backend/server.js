2b17741d70634cfd73e66ffa7532c52afa2f0bcb
ArnavSaikia <arnavsaikia817@gmail.com>
2026-04-11 13:15:25 +0530

    Atlas code seperated into module

--- backend/server.js
+++ backend/server.js
@@ -1,9 +1,11 @@
 require("dotenv").config();
-const mongoose = require('mongoose');
+const connectDB = require("./utils/connectDB");
 const app = require("./app");
 
 const PORT = process.env.PORT || 5000;
 
-mongoose.connect(process.env.MONGO_URI)
-    .then(() => app.listen(3000, console.log(`Server up on ${PORT}`)))
-    .catch((err) => console.log(err));
+connectDB().then(() => {
+    app.listen(PORT, () => {
+        console.log(`Server up on ${PORT}`);
+    });
+});
